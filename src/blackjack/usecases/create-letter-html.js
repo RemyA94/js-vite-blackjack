a637632d5d3aea3099bd730f8489b8b0cf2161bb
RemyA94 <remyjoseacostamelo@gmail.com>
2023-01-23 20:23:33 -0400

    ⚒

--- src/blackjack/usecases/create-letter-html.js
+++ src/blackjack/usecases/create-letter-html.js
@@ -7,7 +7,7 @@ export const crearCartaHtml = (carta) => {
     if ( !carta ) throw new Error('La carta es un argumento obligatorio')
     
     const imgCarta = document.createElement('img');
-    imgCarta.src = `/public/assets/cartas/${carta}.png`; //3H, JD
+    imgCarta.src = `/js-vite-blackjack/docs/assets/cartas/${carta}.png`; //3H, JD
     imgCarta.classList.add('carta');
 
     return imgCarta;
